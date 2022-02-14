9bcd46d06eab52ace73afba9ba30dd139aff5130
Andrey Lugovtsov <andrey.lugovtsov@gmail.com>
2022-02-14 18:42:56 +0000

    lecture-00 hello world

--- solution.go
+++ solution.go
@@ -1,5 +1,8 @@
-package solution
+package main
 
-func GetMessage() string {
-	return ""
+// https://pkg.go.dev/github.com/kyokomi/emoji/v2
+import "github.com/kyokomi/emoji/v2"
+
+func main() {
+	emoji.Println("Hello :world_map:!")
 }
